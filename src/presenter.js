b87c50fad5e5cf1696f462830f9b8833ca5634fc
Rubén Cordero <garu1812@gmail.com>
2026-02-11 23:17:55 -0400

    multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,7 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
+// Sumador
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
@@ -12,4 +14,19 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+// Multiplicador
+const firstMultiplicador = document.querySelector("#primer-numero-multiplicador");
+const secondMultiplicador = document.querySelector("#segundo-numero-multiplicador");
+const formMultiplicador = document.querySelector("#multiplicar-form");
+const divMultiplicador = document.querySelector("#resultado-multiplicador-div");
+
+formMultiplicador.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstMultiplicador.value);
+  const secondNumber = Number.parseInt(secondMultiplicador.value);
+
+  divMultiplicador.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
